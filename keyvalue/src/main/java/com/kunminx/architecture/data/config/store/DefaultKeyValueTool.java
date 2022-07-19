5f038cfdb24d3aa7b86873e339274ac9ad126991
syxc <gaibing2009@gmail.com>
2022-07-19 18:12:33 +0800

    Modified

--- keyvalue/src/main/java/com/kunminx/architecture/data/config/store/DefaultKeyValueTool.java
+++ keyvalue/src/main/java/com/kunminx/architecture/data/config/store/DefaultKeyValueTool.java
@@ -1,6 +1,5 @@
 package com.kunminx.architecture.data.config.store;
 
-import android.app.Application;
 import androidx.annotation.NonNull;
 import com.kunminx.architecture.data.config.utils.SPUtils;
 
@@ -10,10 +9,6 @@ import com.kunminx.architecture.data.config.utils.SPUtils;
 public class DefaultKeyValueTool implements KeyValueTool {
 
   private SPUtils mSPUtils;
-
-  @Override public void init(@NonNull Application application) {
-    // ignore
-  }
 
   public void init(@NonNull String moduleName) {
     mSPUtils = SPUtils.getInstance(moduleName);
